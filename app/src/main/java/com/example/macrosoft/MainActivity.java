a2286cfdca89a89f17510f5ef0fd6fdda2f4fc50
tislind <dmitry_tislin@yahoo.com>
2019-04-25 17:30:04 -0400

    adding database, still broken

--- app/src/main/java/com/example/macrosoft/MainActivity.java
+++ app/src/main/java/com/example/macrosoft/MainActivity.java
@@ -5,9 +5,22 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 }
